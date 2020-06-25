9e31f37d12f038595fcec7f8997bac43f1b49114
RyanSalam <52726747+RyanSalam@users.noreply.github.com>
2020-06-25 01:06:51 -0400

    Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float minTurnSpeed = 400f;
     [SerializeField] private float maxTurnSpeed = 1200f;
     [SerializeField] private float idleTimeout = 5f;
-    [SerializeField] private bool  canAttack;
+    private bool canAttack;
+
+    private float desiredForwardSpeed;
+    private float forwardSpeed;
+    private float verticalSpeed;
+
+    const float k_GroundAcceleration = 20f;
+    const float k_GroundDeceleration = 25f;
 
     private Vector2 MoveDirection;
 
@@ -21,7 +28,7 @@ public class PlayerController : MonoBehaviour
     {
         get
         {
-            return Mathf.Approximately(MoveDirection.magnitude, 0);
+            return !Mathf.Approximately(MoveDirection.sqrMagnitude, 0);
         }
     }
     private bool IsAttacking
@@ -31,6 +38,8 @@ public class PlayerController : MonoBehaviour
             return true;
         }
     }
+
+    private float idleTimer = 0f;
 
     private void Start()
     {
@@ -47,16 +56,80 @@ public class PlayerController : MonoBehaviour
             Debug.Log("attacking");
             anim.SetTrigger("Attack");
         }
+
+        anim.SetBool("Grounded", cc.isGrounded);
     }
 
     private void FixedUpdate()
     {
         anim.SetFloat("StateTime", Mathf.Repeat(anim.GetCurrentAnimatorStateInfo(0).normalizedTime, 1f));
+
+        CalculateForwardMovement();
+
+        Move();
+
+        TimeOutToIdle();
     }
 
-    private void CalculateForwardMovement()
+    private float CalculateForwardMovement()
     {
         if (MoveDirection.sqrMagnitude > 1f)
             MoveDirection.Normalize();
+
+        desiredForwardSpeed = MoveDirection.magnitude * maxForwardSpeed;
+
+        float acceleration = IsMoving ? k_GroundAcceleration : k_GroundDeceleration;
+
+        forwardSpeed = Mathf.MoveTowards(forwardSpeed, desiredForwardSpeed, acceleration * Time.deltaTime);
+
+        anim.SetFloat("ForwardSpeed", forwardSpeed);
+
+        return forwardSpeed;
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(MoveDirection.x, 0, MoveDirection.y);
+
+        cc.Move(movement * forwardSpeed * Time.deltaTime);
+    }
+
+    private void OnAnimatorMove()
+    {
+        Vector3 movement;
+
+        movement = anim.deltaPosition;
+
+        movement += forwardSpeed * transform.forward * Time.deltaTime;
+
+        Debug.Log("move");
+
+        cc.Move(movement);
+    }
+
+    private void TimeOutToIdle()
+    {
+        bool inputDetected = IsMoving || Input.GetMouseButtonDown(0);
+
+        Debug.Log(inputDetected);
+
+        if (!inputDetected)
+        {
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= idleTimeout)
+            {
+                idleTimer = 0f;
+                anim.SetTrigger("Idle");
+                Debug.Log("Going to Idle State");
+            }
+        }
+        else
+        {
+            idleTimer = 0f;
+            anim.ResetTrigger("Idle");
+        }
+
+        //m_Animator.SetBool(m_HashInputDetected, inputDetected);
     }
 }
