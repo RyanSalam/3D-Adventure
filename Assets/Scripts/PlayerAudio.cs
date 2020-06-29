025792ecd799d7b5e63a2d8148a88d5cde141461
RyanSalam <52726747+RyanSalam@users.noreply.github.com>
2020-06-29 02:50:51 -0400

    Audio

--- Assets/Scripts/PlayerAudio.cs
+++ Assets/Scripts/PlayerAudio.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] private List<AudioClip> Footsteps = new List<AudioClip>();
+    [SerializeField] private List<AudioClip> SwordSwings = new List<AudioClip>();
 
     private AudioSource source;
 
@@ -17,6 +18,14 @@ public class PlayerAudio : MonoBehaviour
     {
         int rand = Random.Range(0, Footsteps.Count);
         AudioClip clip = Footsteps[rand];
+
+        source.PlayOneShot(clip);
+    }
+
+    private void PlaySwordSwing()
+    {
+        int rand = Random.Range(0, SwordSwings.Count);
+        AudioClip clip = SwordSwings[rand];
 
         source.PlayOneShot(clip);
     }
